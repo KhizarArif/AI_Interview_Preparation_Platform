27147632a0c25b5bb9fadfa260a853861aac956e
KhizarArif <khizararif201@gmail.com>
2025-11-03 23:03:27 +0500

    Working on Authentication

--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -1,6 +1,10 @@
 "use server";
 
-import { db } from "@/firebase/admin"; 
+import { db, auth } from "@/firebase/admin";  
+import { cookies } from "next/headers";
+
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
 
 export const signUp = async (params: SignUpParams) => {
 
@@ -20,12 +24,57 @@ export const signUp = async (params: SignUpParams) => {
         }
 
         await db.collection('users').doc(uid).set({
-            name, email 
+            name, email, password
         })
 
+        return {
+            success: true,
+            message: "Account created successfully! Please sign in."
+        }
         
     } catch (error) {
         console.error("Error in Action Auth Signup", error);
         
     }
 };
+
+
+export const signIn = async (params: SignInParams) => {
+    const {email, idToken} = params;
+
+    try {
+        
+        const userRecord = await auth.getUserByEmail(email);
+
+        if(!userRecord){
+            return {
+                success: false,
+                message: "User not found"
+            }
+        }
+
+        await setSessionCookies(idToken);
+        
+    } catch (error) {
+        console.log('error', error);
+        
+    }
+
+}
+
+export const setSessionCookies = async (idToken: string) => {
+
+    const cookieStore = await cookies()
+
+    const sessionCookies = await auth.createSessionCookie(idToken, {
+        expiresIn: ONE_WEEK * 1000
+    });
+
+    cookieStore.set("session", sessionCookies, {
+        maxAge: ONE_WEEK,
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: true,
+        sameSite: "lax"
+    })
+
+}
