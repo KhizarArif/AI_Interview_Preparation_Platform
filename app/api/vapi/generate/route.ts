2d85059891548c33a31512032985df2e04a12c89
KhizarArif <khizararif201@gmail.com>
2025-11-14 17:50:53 +0500

    Working on POST function

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,3 +1,66 @@
+
+import { generateText } from 'ai';
+import { google } from "@ai-sdk/google"
+import { db } from '@/firebase/admin';
+import { getRandomInterviewCover } from '@/lib/utils';
+
 export async function GET() {
-    return Response.json({ success: true, message: "Thank you!" }, {status: 200});
+    return Response.json({ success: true, message: "Thank you!" }, { status: 200 });
 }
+
+export async function POST(request: Request) {
+    const { amount, techstack, role, type, userid, level } = await request.json();
+
+    try {
+
+        const { text: questions } = await generateText({
+            model: google('gemini-2.0-flash-001'),
+            prompt: `Prepare questions for a job interview.
+                The job role is ${role}.
+                The job experience level is ${level}.
+                The tech stack used in the job is: ${techstack}.
+                The focus between behavioural and technical questions should lean towards: ${type}.
+                The amount of questions required is: ${amount}.
+                Please return only the questions, without any additional text.
+                The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+                Return the questions formatted like this:
+                ["Question 1", "Question 2", "Question 3"]
+                
+                Thank you! <3
+            `,
+        })
+
+        if (!userid) {
+            return Response.json(
+                { success: false, message: "User ID is required" },
+                { status: 400 }
+            );
+        }
+
+
+
+        const interview = {
+            level, role, type, techstack:
+                techstack.split(','),
+            userId: userid ?? null,
+            amount,
+            questions: JSON.parse(questions),
+            coverImage: getRandomInterviewCover(),
+            finalized: true,
+            createdAt: new Date().toISOString()
+        }
+
+        await db.collection('interviews').add(interview);
+
+        // i also want to return data to the user
+
+        return Response.json({ success: true, message: "Success", data: interview }, { status: 200 });
+
+    } catch (error) {
+        console.log("Error", error);
+
+        return Response.json({ success: false, message: "Internal Server Error" }, { status: 500 });
+
+    }
+
+} 
